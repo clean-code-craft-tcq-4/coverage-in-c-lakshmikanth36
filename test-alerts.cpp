52e89df67c4d52ce4b006877303f842aadf3dcfd
Lakshmikanth Routhu <lakshmikanthr36@gmail.com>
2022-07-20 21:30:03 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -26,5 +26,7 @@ TEST_CASE("Check and Alert of coolingType Temperature") {
   BatteryCharacter batteryChar;
   batteryChar.coolingType = PASSIVE_COOLING;
   assert(checkAndAlert(TO_CONTROLLER,batteryChar,0));
+   assert(checkAndAlert(TO_CONTROLLER,batteryChar,10));
+  assert(checkAndAlert(TO_EMAIL,batteryChar,20));		
   assert(checkAndAlert(TO_EMAIL,batteryChar,35));		
 }
